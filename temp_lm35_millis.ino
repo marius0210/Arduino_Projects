67c141c88c80e86e0760cb254bf519c1d0def670
marius0210 <34185533+marius0210@users.noreply.github.com>
2018-07-23 08:47:59 +0300

    4. Temperature LM35 using milis function
    Components:
    Arduino Uno/Mega etc.
    Temperature Sensor LM35

---
+++ temp_lm35_millis.ino
@@ -0,0 +1,26 @@
+int temp_sensor= A0;
+int temp_reading;
+long previousMillis_temp=0;
+long interval_temp =1000;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+       unsigned long currentMillis = millis();
+
+if(currentMillis - previousMillis_temp > interval_temp) {
+        previousMillis_temp = currentMillis;
+        temp_reading=analogRead (temp_sensor);   
+        float mv = (temp_reading/1024.0)*5000; 
+        float cel = mv/10;
+        float farh = (cel*9)/5 + 32;
+        Serial.print("TEMPRATURE = ");
+        Serial.print(cel);
+        Serial.print("*C");
+        Serial.println();
+  }
+}
